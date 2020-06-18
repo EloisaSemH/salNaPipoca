99df07357e39fe6ae80083a073f0cc07734084a3
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2020-06-18 15:19:05 -0300

    solucionados erros 3 e 4
    Os textos dos links agora estão mais intuitivos / os alt das imagens agora descrevem-nas melhor

--- pags/noticias/cards/card5.php
+++ pags/noticias/cards/card5.php
@@ -5,15 +5,15 @@
                 <div class="card-body">
                     <section class="pl-0 justify-content-center">
                         <div class="text-center d-none d-md-none d-lg-block d-xl-block ml-n3 p-0">
-                            <img src="images/noticias/n5-AnimaisF-g.jpg" alt="Imagem que representa a quinta noticia">
+                            <img src="images/noticias/n5-AnimaisF-g.jpg" alt="Protagonista no alto de um prédio olhando pro vazio">
                         </div>
                         <div class="float-left d-md-block d-lg-none d-xl-none mr-3 ml-3">
-                            <img src="images/noticias/n5-AnimaisF-p.jpg" alt="Imagem que representa a quinta noticia">
+                            <img src="images/noticias/n5-AnimaisF-p.jpg" alt="Protagonista no alto de um prédio olhando pro vazio">
                         </div>
                         <h2 class="card-title">Fantásticos em BR</h3>
                             <p class="card-text mr-n3">Novo filme da franquia de Harry Potter agora terá cenas gravadas no Brasil. Será que chegou a hora do Brasil roubar a cena?</p>
                             <hr>
-                            <a href="#" class="card-link" data-toggle="modal" data-target="#modalFantastico">Saiba Mais</a>
+                            <a href="#" class="card-link" data-toggle="modal" data-target="#modalFantastico">Clique para saber maisProtagonista no alto de um prédio olhando pro vazioa>
                             <!-- MODAL -->
                             <?php
                             include('pags/noticias/modalFantastico.html');
--- pags/noticias/cards/card6.php
+++ pags/noticias/cards/card6.php
@@ -5,15 +5,15 @@
                 <div class="card-body">
                     <section class="pl-0 justify-content-center">
                         <div class="text-center d-none d-md-none d-lg-block d-xl-block ml-n3 p-0">
-                            <img src="images/noticias/n6-Coringa-g.jpg" alt="Imagem que representa a primeira noticia">
+                            <img src="images/noticias/n6-Coringa-g.jpg" alt="Protagonista do filme dançando muito feliz em uma escadaria">
                         </div>
                         <div class="float-left d-md-block d-lg-none d-xl-none mr-3 ml-3">
-                            <img src="images/noticias/n6-Coringa-p.jpg" alt="Imagem que representa a primeira noticia">
+                            <img src="images/noticias/n6-Coringa-p.jpg" alt="Protagonista do filme dançando muito feliz em uma escadaria">
                         </div>
                         <h2 class="card-title">Palhaço na liderança</h3>
                             <p class="card-text mr-n3">Novo filme do universo DC se torna sucesso nas bilheterias do Brasil. Agora a piada se tornou mortalmente lucrativa (se é que me entendem...)</p>
                             <hr>
-                            <a href="#" class="card-link" data-toggle="modal" data-target="#modalCoringa">Saiba Mais</a>
+                            <a href="#" class="card-link" data-toggle="modal" data-target="#modalCoringa">Clique para saber mais</a>
                             <!-- MODAL -->
                             <?php
                             include('pags/noticias/modalCoringa.html');
